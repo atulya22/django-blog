ba271aa8d47cb37aac26338fe0becb4f0b581378
atulya22 <atulya.shetty@hotmail.com>
2020-05-28 14:51:26 -0400

    Adding dummy blog posts Setting up django templates Adding base.html templates

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+    {
+        'author': 'James Vick',
+        'title': 'Django Blog Post',
+        'content': ' This is a Django blog post',
+        'date_posted': 'May 28, 2020'
+    },
+    {
+        'author': 'Tony Ferguson',
+        'title': 'Another Django Blog Post',
+        'content': ' This is another Django blog post',
+        'date_posted': 'May 28, 2020'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1> Blog Home </h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1> Blog About </h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
